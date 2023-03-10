612ca17fa2a45b206dc19f1ecaed22cfebf579f7
LufWang <wang.lufei@mayo.edu>
2023-03-10 19:45:57 +0000

    debug model module

--- pytorch_nlp_pipeline/model/__init__.py
+++ pytorch_nlp_pipeline/model/__init__.py
@@ -1,6 +1,8 @@
 from transformers import BertModel, BertTokenizer, BioGptTokenizer, BioGptModel
 import logging
 from torch import nn
+from collections import OrderedDict
+
 
 WORKER = '[bold]ModelModule[/bold]'
 
@@ -24,23 +26,64 @@ class ModelHead(nn.Module):
         return outputs
 
 
+
+def get_ModelHead(input_size=512, hidden_size=None, num_classes=1):
+        seq_layers = []
+        layer_num_counter = 1
+
+        if type(hidden_size) is not list:
+            hidden_size = [hidden_size]
+
+        clf_head_layers = hidden_size + [num_classes]
+
+        
+        for layer in clf_head_layers:
+            if layer == 'relu':
+                seq_layers.append((f'out{layer_num_counter}_relu', nn.ReLU()))
+            else:
+                seq_layers.append((f'out{layer_num_counter}_linear', nn.Linear(input_size, layer)))
+                input_size = layer
+        
+            layer_num_counter+=1
+
+        logging.info(f'{WORKER}: Classfication Head Added. Number of Hidden Layers - {len(seq_layers)}. Number of Classes - {num_classes}')
+
+        
+        return nn.Sequential(OrderedDict(seq_layers))
+
+
+            
+    
+ 
+
 class PytorchNlpModel(nn.Module):
-    def __init__(self, pretrained_type, pretrained_path, n_classes, freeze_pretrained=True, head_hidden_size=512):
+    def __init__(self, 
+                 pretrained_type, 
+                 pretrained_path, 
+                 device,
+                 n_classes, 
+                 freeze_pretrained=True, 
+                 head_hidden_size=[384, 'relu']
+
+                 ):
+        
         logging.info(f'{WORKER}: PytorchNlpModel initiating...')
         super(PytorchNlpModel, self).__init__()
         self.pretrained_type = pretrained_type
+
         if pretrained_type == 'BERT':
             self.tokenizer = BertTokenizer.from_pretrained(pretrained_path)
             self.pretrained = BertModel.from_pretrained(pretrained_path)
         elif pretrained_type == 'BioGPT':
             self.tokenizer = BioGptTokenizer.from_pretrained(pretrained_path)
             self.pretrained = BioGptModel.from_pretrained(pretrained_path)
+
         logging.info(f'{WORKER}: tokenizer and pretrained for {pretrained_type} loaded.')
         self.pretrained_path = pretrained_path
         self.freeze_pretrained = freeze_pretrained
         self.drop = nn.Dropout(p=0.3)
         self.n_classes = n_classes
-        self.head = ModelHead(self.pretrained.config.hidden_size,
+        self.head = get_ModelHead(self.pretrained.config.hidden_size,
                                       head_hidden_size,
                                       self.n_classes)
         if freeze_pretrained:
@@ -50,8 +93,6 @@ class PytorchNlpModel(nn.Module):
         logging.info(f'{WORKER}: PytorchNlpModel of type {pretrained_type} with classification head initiated.')
         logging.info(f'{WORKER}: pretrained model freezed - {freeze_pretrained}')
 
-    def load_weights(self):
-        pass
     
     def forward(self, input_ids, attention_mask):
         outputs  = self.pretrained(input_ids = input_ids, 
